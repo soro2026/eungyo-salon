0ec6db1afc23107c81faaa802b59f2bea23459ee
soro2026 <jacobletter906@gmail.com>
2026-04-05 11:16:31 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = 'eungyo-v1';
+const FILES = [
+  '/eungyo-salon/',
+  '/eungyo-salon/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
