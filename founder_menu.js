0deecdfc95c592c6de2330105593904884a648f9
soro2026 <jacobletter906@gmail.com>
2026-09-18 16:49:36 +0900

    Add files via upload

--- founder_menu.js
+++ founder_menu.js
@@ -40,7 +40,7 @@
   if (window.__egFounderMenu) return;
   window.__egFounderMenu = true;
 
-  const V = '0918h';
+  const V = '0918i';
   const GUIDE = 'founder_guide.html?embed=1&v=' + V;
   const CITY  = 'founder_city.html?embed=1&v=' + V;
   const CONT  = 'founder_contract.html?embed=1&v=' + V;
